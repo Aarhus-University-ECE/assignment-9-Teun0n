7b7c95b2e87a38773a44cb4e6caa226b894a72d7
Teun0n <simon.a.andreasen@gmail.com>
2022-11-03 12:51:04 +0100

    sum complete comments added.

--- src/sum.c
+++ src/sum.c
@@ -1,6 +1,24 @@
 #include "sum.h"
+#include <stdio.h>
+#include <assert.h>
 
 int sum(int a[], int n)
 {
-    return 0;
+    //pre: n>=0
+    //post: sum of all integers in array is returned.
+    assert(n>=0);//n cant be less than 0.
+
+    int sum = 0;//sum is set to have start value 0.
+
+    if(n>0)//if n is zero array is empty sum of arrays is 
+    {
+        for(int i=0;i<n;i++)//all values in array are summed:
+        {
+            sum=sum+a[i];
+        }
+    }
+    else //returns sum which is set to 0 if n=0.
+    
+    return sum;//sum is returned
+    
 }
